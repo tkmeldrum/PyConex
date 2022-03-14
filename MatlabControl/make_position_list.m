9d9d1f0b554f0e49640c612028134fce0f143a9c
Bigmagnet <tkmeldrum@wm.edu>
2022-03-14 13:31:19 -0400

    update pyconnex with new params

--- MatlabControl/make_position_list.m
+++ MatlabControl/make_position_list.m
@@ -13,20 +13,20 @@ close all
 % user defined parameters
 actuator_lims = [0 12];
 
-tilts = linspace(-250,300,17); %linspace(-260,300,15); %um
-tips =  linspace(-320,293,17); %linspace(-260,300,15); %um
-centroids = 6.70; %6.65; %mm
+tilts = 0; %linspace(-260,300,15); %um
+tips =  0; %linspace(-260,300,15); %um
+centroids = linspace(0,3,31) %6.65; %mm
 
-filedir = 'Z:\Data\LJK\PM5\November2021\Sample174\CPMGSeriesA\';
+filedir = 'Z:\Data\TKM\PM5\Mar2022\RubberBlock\CPMG_Series\';
 
 positions = make_positions_mesh(tilts,tips,centroids);
 
-% positions = [(1:22)',ones(22,1)*centroids, tilts, tips];
+%positions = [(1:22)',ones(22,1)*centroids, tilts, tips];
 
 [positions, abs_pos] = check_valid_actuator_moves(positions,actuator_lims);
 %%
 write_input_positions(filedir,positions)
-plot_actuator_limits(positions,actuator_lims)
+%plot_actuator_limits(positions,actuator_lims)
 
 
 
--- test_file_read.py
+++ test_file_read.py
@@ -3,9 +3,9 @@ from move_multiple import ConexGroup
 import numpy as np
 import time, datetime, serial
 
-filename_in = r"Z:\Data\LJK\PM5\November2021\Sample174\CPMGSeriesA\input_positions.txt"
-filename_out = r"Z:\Data\LJK\PM5\November2021\Sample174\CPMGSeriesA\output_positions.txt"
-timestamps_out = r"Z:\Data\LJK\PM5\November2021\Sample174\CPMGSeriesA\stamps.txt"
+filename_in = r"Z:\Data\TKM\PM5\Mar2022\RubberBlock\CPMG_Series\input_positions.txt"
+filename_out = r"Z:\Data\TKM\PM5\Mar2022\RubberBlock\CPMG_Series\output_positions.txt"
+timestamps_out = r"Z:\Data\TKM\PM5\Mar2022\RubberBlock\CPMG_Series\stamps.txt"
 # exptime = 1024*0.35 + 9
 
 serialport = 'COM10'

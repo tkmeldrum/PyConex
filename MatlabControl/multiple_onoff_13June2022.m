94ef005aac61d6ddd13c9922f3bb6b5f291af1a4
Meldrum Lab <tkmeldrum@wm.edu>
2022-06-17 12:08:34 -0400

    add monoexp fitting to onoff proc

--- MatlabControl/multiple_onoff_13June2022.m
+++ MatlabControl/multiple_onoff_13June2022.m
@@ -7,18 +7,18 @@ params.gamma = 42.577; %MHz/T
 params.G = 23.87; %T/m
 T2kernel = 'exp(-h/T)';
 
-figdir = 'Z:\Data\TKM\PM5\June2022\TiltTip\Epoxy31\CPMG_ONOFFBAD1\';
+maindir = '/Volumes/acstore-groups/ISC1026/Data/TKM/PM5/June2022/TiltTip/Epoxy31/CPMG_ONOFFBAD1/';
 
-dirname = 'Z:\Data\TKM\PM5\June2022\TiltTip\Epoxy31\CPMG_ONOFFBAD1\ON\';
+dirname = [maindir,'ON/'];
 ON = runmultipleonoff_tilttip(dirname,params,T2kernel);
 
-dirname = 'Z:\Data\TKM\PM5\June2022\TiltTip\Epoxy31\CPMG_ONOFFBAD1\OFF\';
+dirname = [maindir,'OFF/'];
 OFF = runmultipleonoff_tilttip(dirname,params,T2kernel);
 
-dirname = 'Z:\Data\TKM\PM5\June2022\TiltTip\Epoxy31\CPMG_ONOFFBAD1\REV\';
+dirname = [maindir,'REV/'];
 REV = runmultipleonoff_tilttip(dirname,params,T2kernel);
 
-dirname = 'Z:\Data\TKM\PM5\June2022\TiltTip\Epoxy31\CPMG_ONOFFBAD1\BAD\';
+dirname = [maindir,'BAD/'];
 BAD = runmultipleonoff_tilttip(dirname,params,T2kernel);
 
 %%
@@ -27,33 +27,33 @@ set(gcf,'Position',[1 1 560  800])
 subplot(2,1,2)
 hold on
 for ii = 1:ON.nDir
-    contour(ON.z_vec,ON.tauh,squeeze(ON.spectrum(:,:,ii)),'red','LineWidth',0.5)
+    contour(ON.z_vec,ON.tauh,squeeze(ON.spectrum(:,:,ii)),10,'red','LineWidth',0.5)
 end
-plot(ON.z_vec,ON.MLT2,':r','LineWidth',1)
+% plot(ON.z_vec,ON.MLT2,':r','LineWidth',1)
 plot(ON.z_vec,ON.meanMLT2,'-r','LineWidth',2)
 
 for ii = 1:OFF.nDir
-    contour(OFF.z_vec,OFF.tauh,squeeze(OFF.spectrum(:,:,ii)),'black')
+    contour(OFF.z_vec,OFF.tauh,squeeze(OFF.spectrum(:,:,ii)),10,'black','LineWidth',0.5)
 end
-plot(OFF.z_vec,OFF.MLT2,':k','LineWidth',1)
+% plot(OFF.z_vec,OFF.MLT2,':k','LineWidth',1)
 plot(OFF.z_vec,OFF.meanMLT2,'-k','LineWidth',2)
 
-for ii = 1:REV.nDir
-    contour(REV.z_vec,REV.tauh,squeeze(REV.spectrum(:,:,ii)),'blue','LineWidth',0.5)
-end
-plot(REV.z_vec,REV.MLT2,':b','LineWidth',1)
-plot(REV.z_vec,REV.meanMLT2,'-b','LineWidth',2)
+% for ii = 1:REV.nDir
+%     contour(REV.z_vec,REV.tauh,squeeze(REV.spectrum(:,:,ii)),5,'blue','LineWidth',0.5)
+% end
+% plot(REV.z_vec,REV.MLT2,':b','LineWidth',1)
+% plot(REV.z_vec,REV.meanMLT2,'-b','LineWidth',2)
 
-for ii = 1:BAD.nDir
-    contour(BAD.z_vec,BAD.tauh,squeeze(BAD.spectrum(:,:,ii)),'green','LineWidth',0.5)
-end
-plot(BAD.z_vec,BAD.MLT2,':g','LineWidth',1)
-plot(BAD.z_vec,BAD.meanMLT2,'-g','LineWidth',2)
+% for ii = 1:BAD.nDir
+%     contour(BAD.z_vec,BAD.tauh,squeeze(BAD.spectrum(:,:,ii)),5,'green','LineWidth',0.5)
+% end
+% plot(BAD.z_vec,BAD.MLT2,':g','LineWidth',1)
+% plot(BAD.z_vec,BAD.meanMLT2,'-g','LineWidth',2)
 
 set(gca,'YScale','log')
 xlabel('position [µm]')
-xlim([0 75])
-% xlim(ON.params.zlims)
+% xlim([0 75])
+xlim(ON.params.zlims)
 ylabel('T2 [s]')
 
 % pubgraph(hh)
@@ -62,10 +62,10 @@ ylabel('T2 [s]')
 % gg = figure(2);
 subplot(2,1,1)
 hold on
-plot(ON.z,mean(squeeze(abs(sum(ON.spatialdata,2))),2),'-r')
-plot(OFF.z,mean(squeeze(abs(sum(OFF.spatialdata,2))),2),'-k')
-plot(REV.z,mean(squeeze(abs(sum(REV.spatialdata,2))),2),'-b')
-plot(BAD.z,mean(squeeze(abs(sum(BAD.spatialdata,2))),2),'-g')
+plot(ON.z,mean(squeeze(abs(sum(ON.spatialdata,2))),2,'omitnan'),'-r')
+plot(OFF.z,mean(squeeze(abs(sum(OFF.spatialdata,2))),2,'omitnan'),'-k')
+plot(REV.z,mean(squeeze(abs(sum(REV.spatialdata,2))),2,'omitnan'),'-b')
+plot(BAD.z,mean(squeeze(abs(sum(BAD.spatialdata,2))),2,'omitnan'),'-g')
 % plot(ON.z_vec,log10(ON.meanMLT2)-log10(OFF.meanMLT2),'-k','LineWidth',2)
 % line(ON.params.zlims,[0 0],'Color','blue')
 % xlabel('position [µm]')
@@ -74,4 +74,63 @@ xlim(ON.params.zlims)
 ylabel('signal intensity [arb]')
 pubgraph(hh)
 
-% print([figdir,'ONOFFcomp.png'],'-dpng');
+print([maindir,'ONOFFILTcomp.png'],'-dpng');
+
+%%
+close all
+gg = figure(2);
+subplot(2,1,1)
+hold on
+% plot(ON.z_vec,ON.monoexp.T2,'-r')
+% plot(ON.z_vec,mean(ON.monoexp.T2,2),'-r','LineWidth',2)
+for ii = 1:3
+    plot(ON.z_vec,ON.monoexp.T2(:,ii),'.-r')
+    plot(ON.z_vec,ON.monoexp.T2(:,ii)-ON.monoexp.T2ci(:,ii),':r')
+    plot(ON.z_vec,ON.monoexp.T2(:,ii)+ON.monoexp.T2ci(:,ii),':r')
+    
+    plot(OFF.z_vec,OFF.monoexp.T2(:,ii),'.-k')
+    plot(OFF.z_vec,OFF.monoexp.T2(:,ii)-OFF.monoexp.T2ci(:,ii),':k')
+    plot(OFF.z_vec,OFF.monoexp.T2(:,ii)+OFF.monoexp.T2ci(:,ii),':k')
+    
+    plot(REV.z_vec,REV.monoexp.T2(:,ii),'.-b')
+    plot(REV.z_vec,REV.monoexp.T2(:,ii)-REV.monoexp.T2ci(:,ii),':b')
+    plot(REV.z_vec,REV.monoexp.T2(:,ii)+REV.monoexp.T2ci(:,ii),':b')
+end
+% plot(OFF.z_vec,OFF.monoexp.T2,'-k')
+% plot(OFF.z_vec,mean(OFF.monoexp.T2,2),'-k','LineWidth',2)
+% plot(OFF.z_vec,mean(OFF.monoexp.T2,2)-min(OFF.monoexp.T2ci,[],2),':k')
+% plot(OFF.z_vec,mean(OFF.monoexp.T2,2)+max(OFF.monoexp.T2ci,[],2),':k')
+
+% plot(REV.z_vec,REV.monoexp.T2,'-b')
+% plot(REV.z_vec,mean(REV.monoexp.T2,2),'-b','LineWidth',2)
+% plot(REV.z_vec,mean(REV.monoexp.T2,2)-min(REV.monoexp.T2ci,[],2),':b')
+% plot(REV.z_vec,mean(REV.monoexp.T2,2)+max(REV.monoexp.T2ci,[],2),':b')
+
+% plot(BAD.z_vec,BAD.monoexp.T2,'-g')
+
+xlim([-10 10])
+ylim([0.01 0.05])
+xlabel('position [µm]')
+ylabel('monexponential fit T2 [s]')
+% set(gca,'YScale','log')
+
+
+subplot(2,1,2)
+hold on
+for ii = 1:3
+    plot(ON.z_vec,ON.monoexp.A(:,ii),'.-r')
+    plot(ON.z_vec,ON.monoexp.A(:,ii)-ON.monoexp.Aci(:,ii),':r')
+    plot(ON.z_vec,ON.monoexp.A(:,ii)+ON.monoexp.Aci(:,ii),':r')
+    
+    plot(OFF.z_vec,OFF.monoexp.A(:,ii),'.-k')
+    plot(OFF.z_vec,OFF.monoexp.A(:,ii)-OFF.monoexp.Aci(:,ii),':k')
+    plot(OFF.z_vec,OFF.monoexp.A(:,ii)+OFF.monoexp.Aci(:,ii),':k')
+    
+    plot(REV.z_vec,REV.monoexp.A(:,ii),'.-b')
+    plot(REV.z_vec,REV.monoexp.A(:,ii)-REV.monoexp.Aci(:,ii),':b')
+    plot(REV.z_vec,REV.monoexp.A(:,ii)+REV.monoexp.Aci(:,ii),':b')
+end
+ylim([-0.5 1])
+
+xlim([-50 10])
+pubgraph(gg)
--- MatlabControl/runmultipleonoff_tilttip.m
+++ MatlabControl/runmultipleonoff_tilttip.m
@@ -7,25 +7,49 @@ for ii = 1:data.nDir
     [data.echoVec,data.z,data.spatialdata(:,:,ii),data.timedata(:,:,ii),data.params] = readKeaForFTT2(fulldir,params.G, params.gamma, params.zf);
 end
 
-data.params.T2_minmax = [1e-3 1e0];
-data.params.T2_steps = 100;
+data.params.T2_minmax = [1e-4 1e1];
+data.params.T2_steps = 120;
 data.params.alpha = 1e8;
-data.params.zlims = [-100 100];
+data.params.zlims = [-50 50];
 
 data.params.z_ind = [find(data.z<data.params.zlims(1),1,'last') find(data.z>data.params.zlims(2),1,'first')];
 data.z_vec = data.z(data.params.z_ind(1):data.params.z_ind(2))';
 
+% remove y-offset
+
+offset_frac = 0.15;
+ft = fittype( 'A*(exp(-x/T2))+y0', 'independent', 'x', 'dependent', 'y' );
+opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+opts.Display = 'Off';
+opts.Lower = [-Inf 0 -Inf];
+opts.StartPoint = [0.6 0.03 0.15];
+
 %%
 for jj = 1:data.nDir
     for ii = 1:range(data.params.z_ind)+1
-        [data.spectrum(:,ii,jj),data.tauh]=upnnlsmooth1D(abs(data.spatialdata(ii+data.params.z_ind(1)-1,:,jj)'),data.echoVec',...
+        offset_range = [numel(data.echoVec)-round(offset_frac*numel(data.echoVec)) numel(data.echoVec)];
+        offset = mean(abs(data.spatialdata(ii+data.params.z_ind(1)-1,offset_range,jj)));
+        [data.spectrum(:,ii,jj),data.tauh]=upnnlsmooth1D(abs(data.spatialdata(ii+data.params.z_ind(1)-1,:,jj)')-offset,data.echoVec',...
             data.params.T2_minmax(1),data.params.T2_minmax(2),data.params.alpha,-1,min(data.params.T2_steps,data.params.nrEchoes),T2kernel);
+        
+        [xData, yData] = prepareCurveData( data.echoVec, abs(data.spatialdata(ii+data.params.z_ind(1)-1,:,jj)) );
+        [fitresult, gof] = fit( xData, yData, ft, opts );
+        aa = coeffvalues(fitresult);
+        ci = range(confint(fitresult))/2;
+        data.monoexp.A(ii,jj) = aa(1);
+        data.monoexp.Aci(ii,jj) = ci(1);
+        data.monoexp.T2(ii,jj) = aa(2);
+        data.monoexp.T2ci(ii,jj) = ci(2);
+        data.monoexp.y0(ii,jj) = aa(3);
+        data.monoexp.y0ci(ii,jj) = ci(3);
+        data.monoexp.rmse(ii,jj) = gof.rmse;
+        
     end
     data.MLT2(:,jj) = mean_log(data.tauh',data.spectrum(:,:,jj));
 end
 
 
 
-data.meanMLT2 = 10.^mean(log10(data.MLT2),2);
+data.meanMLT2 = 10.^mean(log10(data.MLT2),2,'omitnan');
 
 end
